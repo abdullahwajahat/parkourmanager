f8cadf0ea5b724691963f439ca733c8e20f8272e
Abdullah <86512482+abdullahwajahat@users.noreply.github.com>
2025-09-14 23:51:56 +0500

    Update Region.java

--- src/main/java/com/example/parkourregion/managers/Region.java
+++ src/main/java/com/example/parkourregion/managers/Region.java
@@ -1,25 +1,42 @@
 package com.example.parkourregion.managers;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Region {
 
     private String name;
+    private Location pos1, pos2;
     private Location start;
-    private Location end;
     private Map<Integer, Location> checkpoints = new HashMap<>();
+    private Location finish;
+    private Set<Material> blacklist = new HashSet<>();
+    private Integer yLimit;
 
-    public Region(String name) {
-        this.name = name;
-    }
+    public Region(String name) { this.name = name; }
 
     public String getName() { return name; }
+    public Location getPos1() { return pos1; }
+    public void setPos1(Location pos1) { this.pos1 = pos1; }
+    public Location getPos2() { return pos2; }
+    public void setPos2(Location pos2) { this.pos2 = pos2; }
+
     public Location getStart() { return start; }
     public void setStart(Location start) { this.start = start; }
-    public Location getEnd() { return end; }
-    public void setEnd(Location end) { this.end = end; }
+
     public Map<Integer, Location> getCheckpoints() { return checkpoints; }
     public void setCheckpoints(Map<Integer, Location> checkpoints) { this.checkpoints = checkpoints; }
+
+    public Location getFinish() { return finish; }
+    public void setFinish(Location finish) { this.finish = finish; }
+
+    public Set<Material> getBlacklist() { return blacklist; }
+    public void setBlacklist(Set<Material> blacklist) { this.blacklist = blacklist; }
+
+    public Integer getYLimit() { return yLimit; }
+    public void setYLimit(Integer yLimit) { this.yLimit = yLimit; }
 }
