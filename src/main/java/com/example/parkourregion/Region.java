cae2dc10dcd7a47217ff82e6219173d50465e153
Abdullah <86512482+abdullahwajahat@users.noreply.github.com>
2025-09-14 18:30:43 +0500

    Update Region.java

--- src/main/java/com/example/parkourregion/Region.java
+++ src/main/java/com/example/parkourregion/Region.java
@@ -1,59 +1,38 @@
 // File: src/main/java/com/example/parkourregion/Region.java
 package com.example.parkourregion;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Region {
 
-    private final Location start;
-    private final Location end;
-    private final List<String> blacklist;
-
-    public Region(Location start, Location end, List<String> blacklist) {
-        this.start = start;
-        this.end = end;
-        this.blacklist = blacklist;
-    }
-
-    public Location getStart() {
-        return start;
-    }
-
-    public Location getEnd() {
-        return end;
-    }
-
-    public List<String> getBlacklist() {
-        return blacklist;
-    }
+    private Location start;
+    private List<String> blacklist = new ArrayList<>();
 
     public boolean contains(Location loc) {
+        // Example check, implement your region bounds logic
         return loc.getWorld().equals(start.getWorld())
                 && loc.getX() >= start.getX() && loc.getX() <= end.getX()
                 && loc.getY() >= start.getY() && loc.getY() <= end.getY()
                 && loc.getZ() >= start.getZ() && loc.getZ() <= end.getZ();
     }
 
-    public String getStartLocationString() {
-        return locationToString(start);
+    public void setStart(Location start) {
+        this.start = start;
     }
 
-    public String getEndLocationString() {
-        return locationToString(end);
+    public Location getStart() {
+        return start;
     }
 
-    public static String locationToString(Location loc) {
-        return loc.getWorld().getName() + "," + loc.getX() + "," + loc.getY() + "," + loc.getZ();
+    public List<String> getBlacklist() {
+        return blacklist;
     }
 
-    public static Location stringToLocation(String s) {
-        String[] parts = s.split(",");
-        return new Location(Bukkit.getWorld(parts[0]),
-                Double.parseDouble(parts[1]),
-                Double.parseDouble(parts[2]),
-                Double.parseDouble(parts[3]));
+    public void setBlacklist(List<String> blacklist) {
+        this.blacklist = blacklist;
     }
 }
