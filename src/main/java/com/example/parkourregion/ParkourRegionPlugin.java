ee6556a015b4c2a3c477a81b97b77a50ba50bf48
Abdullah <86512482+abdullahwajahat@users.noreply.github.com>
2025-09-14 16:47:13 +0500

    Update ParkourRegionPlugin.java

--- src/main/java/com/example/parkourregion/ParkourRegionPlugin.java
+++ src/main/java/com/example/parkourregion/ParkourRegionPlugin.java
@@ -2,38 +2,22 @@ package com.example.parkourregion;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ParkourRegion extends JavaPlugin {
+public final class ParkourRegionPlugin extends JavaPlugin {
 
-    private static ParkourRegion instance;
     private RegionManager regionManager;
 
     @Override
     public void onEnable() {
-        instance = this;
         saveDefaultConfig();
-
-        // Initialize region manager
         regionManager = new RegionManager(this);
-        regionManager.loadRegions();
 
-        // Register command & tab completer
-        this.getCommand("por").setExecutor(new ParkourCommand(regionManager));
-        this.getCommand("por").setTabCompleter(new ParkourTabCompleter(regionManager));
+        // Register commands
+        getCommand("por").setExecutor(new ParkourCommand(this));
+        getCommand("por").setTabCompleter(new ParkourTabCompleter(this));
 
-        // Register listener
-        getServer().getPluginManager().registerEvents(new MovementListener(regionManager), this);
-
-        getLogger().info("ParkourRegion enabled!");
-    }
-
-    @Override
-    public void onDisable() {
-        regionManager.saveRegions();
-        getLogger().info("ParkourRegion disabled!");
-    }
-
-    public static ParkourRegion getInstance() {
-        return instance;
+        // Register listeners
+        getServer().getPluginManager().registerEvents(new MovementListener(this), this);
+        getServer().getPluginManager().registerEvents(new InteractionListener(this), this);
     }
 
     public RegionManager getRegionManager() {
