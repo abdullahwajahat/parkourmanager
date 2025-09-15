14e7e368d93d3d0889c899dc502ede098e45cd9c
Abdullah <86512482+abdullahwajahat@users.noreply.github.com>
2025-09-15 21:06:25 +0500

    Update MessageUtil.java

--- src/main/java/com/example/parkourmanager/utils/MessageUtil.java
+++ src/main/java/com/example/parkourmanager/utils/MessageUtil.java
@@ -1,9 +1,9 @@
 package com.example.parkourmanager.utils;
+import com.example.parkourmanager.ParkourManager;
 
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import com.example.parkourmanager.ParkourManager;
 
 import java.io.File;
 
