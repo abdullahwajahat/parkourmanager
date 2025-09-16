68d890af8b37574f58d73087cb6d8a1d62ed205a
Abdullah <86512482+abdullahwajahat@users.noreply.github.com>
2025-09-16 17:11:49 +0500

    Update ParkourManager.java

--- src/main/java/com/example/parkourmanager/ParkourManager.java
+++ src/main/java/com/example/parkourmanager/ParkourManager.java
@@ -1,54 +1,46 @@
 package com.example.parkourmanager;
 
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.InvalidConfigurationException;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import com.example.parkourmanager.commands.ParkourCommand;
+import com.example.parkourmanager.data.RegionManager;
+import com.example.parkourmanager.data.SelectionManager;
+import com.example.parkourmanager.utils.MessageUtil;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.File;
-import java.io.IOException;
 
 public class ParkourManager extends JavaPlugin {
 
     private static ParkourManager instance;
-
     private RegionManager regionManager;
     private SelectionManager selectionManager;
-    private FileConfiguration messagesConfig;
-    private File messagesFile;
 
     @Override
     public void onEnable() {
         instance = this;
 
-        // Save default config.yml if not exists
+        // Save default config and messages.yml
         saveDefaultConfig();
+        saveResource("messages.yml", false);
 
-        // Load messages.yml
-        createMessagesFile();
+        // Initialize utils and managers
+        MessageUtil.init(this);
+        this.regionManager = new RegionManager(this);
+        this.selectionManager = new SelectionManager();
 
-        // Init managers
-        regionManager = new RegionManager(this);
-        selectionManager = new SelectionManager();
+        // Register command
+        ParkourCommand cmd = new ParkourCommand(this);
+        getCommand("parkourmanager").setExecutor(cmd);
+        getCommand("parkourmanager").setTabCompleter(cmd);
 
-        // Register commands
-        getCommand("parkourmanager").setExecutor(new ParkourCommand(this));
-        getCommand("parkourmanager").setTabCompleter(new ParkourTabCompleter(this));
-
-        // Register events
-        Bukkit.getPluginManager().registerEvents(new RegionListener(this), this);
-        Bukkit.getPluginManager().registerEvents(new BlacklistListener(this), this);
-
-        getLogger().info("Parkour Manager enabled successfully!");
+        getLogger().info("ParkourManager enabled!");
     }
 
     @Override
     public void onDisable() {
+        // Save regions before shutting down
         regionManager.saveRegions();
-        getLogger().info("Parkour Manager disabled!");
+        getLogger().info("ParkourManager disabled!");
     }
 
+    // Getters
     public static ParkourManager getInstance() {
         return instance;
     }
@@ -59,32 +51,5 @@ public class ParkourManager extends JavaPlugin {
 
     public SelectionManager getSelectionManager() {
         return selectionManager;
-    }
-
-    public FileConfiguration getMessagesConfig() {
-        return messagesConfig;
-    }
-
-    public void saveMessagesConfig() {
-        try {
-            messagesConfig.save(messagesFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void createMessagesFile() {
-        messagesFile = new File(getDataFolder(), "messages.yml");
-        if (!messagesFile.exists()) {
-            messagesFile.getParentFile().mkdirs();
-            saveResource("messages.yml", false);
-        }
-
-        messagesConfig = new YamlConfiguration();
-        try {
-            messagesConfig.load(messagesFile);
-        } catch (IOException | InvalidConfigurationException e) {
-            e.printStackTrace();
-        }
     }
 }
