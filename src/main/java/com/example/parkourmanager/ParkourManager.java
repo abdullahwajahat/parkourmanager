ee74149f6575981e52ca1de9917f1590e3d08490
Abdullah <86512482+abdullahwajahat@users.noreply.github.com>
2025-09-16 20:40:43 +0500

    Update ParkourManager.java

--- src/main/java/com/example/parkourmanager/ParkourManager.java
+++ src/main/java/com/example/parkourmanager/ParkourManager.java
@@ -1,55 +1,40 @@
 package com.example.parkourmanager;
 
 import com.example.parkourmanager.commands.ParkourCommand;
+import com.example.parkourmanager.commands.TabCompletion;
 import com.example.parkourmanager.data.RegionManager;
-import com.example.parkourmanager.data.SelectionManager;
-import com.example.parkourmanager.utils.MessageUtil;
+import com.example.parkourmanager.listeners.PlayerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ParkourManager extends JavaPlugin {
 
-    private static ParkourManager instance;
     private RegionManager regionManager;
-    private SelectionManager selectionManager;
 
     @Override
     public void onEnable() {
-        instance = this;
-
-        // Save default config and messages.yml
         saveDefaultConfig();
-        saveResource("messages.yml", false);
-
-        // Initialize utils and managers
-        MessageUtil.init(this);
         this.regionManager = new RegionManager(this);
-        this.selectionManager = new SelectionManager();
 
-        // Register command
-        ParkourCommand cmd = new ParkourCommand(this);
-        getCommand("parkourmanager").setExecutor(cmd);
-        getCommand("parkourmanager").setTabCompleter(cmd);
+        // Register command executor + tab completer
+        ParkourCommand parkourCommand = new ParkourCommand(regionManager);
+        getCommand("parkour").setExecutor(parkourCommand);
+        getCommand("parkour").setTabCompleter(new TabCompletion(regionManager));
+
+        // Register listener
+        getServer().getPluginManager().registerEvents(new PlayerListener(regionManager), this);
 
         getLogger().info("ParkourManager enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Save regions before shutting down
-        regionManager.saveRegions();
+        if (regionManager != null) {
+            regionManager.saveRegions();
+        }
         getLogger().info("ParkourManager disabled!");
-    }
-
-    // Getters
-    public static ParkourManager getInstance() {
-        return instance;
     }
 
     public RegionManager getRegionManager() {
         return regionManager;
-    }
-
-    public SelectionManager getSelectionManager() {
-        return selectionManager;
     }
 }
