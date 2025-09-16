7032fed2a84b08fbe574618b4ca4ecb45c0f7ffd
Abdullah <86512482+abdullahwajahat@users.noreply.github.com>
2025-09-16 20:41:09 +0500

    Update TabCompletion.java

--- src/main/java/com/example/parkourmanager/commands/TabCompletion.java
+++ src/main/java/com/example/parkourmanager/commands/TabCompletion.java
@@ -1,8 +1,10 @@
 package com.example.parkourmanager.commands;
 
+import com.example.parkourmanager.data.RegionManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,17 +12,34 @@ import java.util.List;
 
 public class TabCompletion implements TabCompleter {
 
+    private final RegionManager regionManager;
+
+    public TabCompletion(RegionManager regionManager) {
+        this.regionManager = regionManager;
+    }
+
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         List<String> completions = new ArrayList<>();
 
+        if (!(sender instanceof Player)) {
+            return completions;
+        }
+
         if (args.length == 1) {
-            completions.addAll(Arrays.asList("pos1", "pos2", "create", "setstart", "setfinish",
+            completions.addAll(Arrays.asList(
+                    "pos1", "pos2", "create", "setstart", "setfinish",
                     "addcheckpoint", "removecheckpoint", "editcheckpoint",
-                    "region", "list", "info", "delete"));
+                    "region", "list", "info", "delete"
+            ));
         } else if (args.length == 2 && args[0].equalsIgnoreCase("region")) {
-            completions.addAll(Arrays.asList("addfinishcommand", "removefinishcommand", "setfinishcommand",
-                    "setfallY", "cooldown", "addblacklist", "removeblacklist", "tp"));
+            completions.addAll(regionManager.getRegionNames());
+        } else if (args.length == 3 && args[0].equalsIgnoreCase("region")) {
+            completions.addAll(Arrays.asList(
+                    "removecheckpoint", "addfinishcommand", "removefinishcommand",
+                    "setfinishcommand", "setfallY", "cooldown",
+                    "addblacklist", "removeblacklist", "tp"
+            ));
         }
 
         return completions;
